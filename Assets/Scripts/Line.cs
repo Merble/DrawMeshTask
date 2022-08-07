8516b8d3f3833ec1a9990e57b41b58df28e3c9aa
Ömer Tekin <tekinomer241@gmail.com>
2022-08-07 21:05:05 +0300

    Initial Commit

---
+++ Assets/Scripts/Line.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+
+public class Line : MonoBehaviour 
+{
+	[SerializeField] private LineRenderer _LineRenderer;
+	[SerializeField] private GameObject _MeshObject;
+
+	private float _pointsMinDistance;
+	private float _distance;
+	private float _angle;
+
+	public int PointsCount { get; private set; }
+
+	public void AddPoint(Vector2 newPoint)
+	{
+		if (PointsCount >= 1 && Vector2.Distance(newPoint, GetLastPoint()) < _pointsMinDistance)  return;
+
+		PointsCount++;
+		
+		_LineRenderer.positionCount = PointsCount;
+		_LineRenderer.SetPosition(PointsCount - 1, newPoint);
+	}
+
+	private Vector2 GetLastPoint()
+	{
+		return _LineRenderer.GetPosition(PointsCount - 1);
+	}
+
+	public void SetLineColor(Gradient colorGradient) 
+	{
+		_LineRenderer.colorGradient = colorGradient;
+	}
+
+	public void SetPointsMinDistance(float distance)
+	{
+		_pointsMinDistance = distance;
+	}
+
+	public void SetLineWidth(float width) 
+	{
+		_LineRenderer.startWidth = width;
+		_LineRenderer.endWidth = width;
+	}
+
+	public void DrawObject(float width, GameObject parentObject)
+	{
+		if (parentObject.transform.childCount < 1)
+		{
+			GenerateObject(width,parentObject);
+		}
+		else
+		{
+			foreach (Transform child in parentObject.transform)
+			{
+				Destroy(child.gameObject);
+			}
+			
+			GenerateObject(width, parentObject);
+		}
+	}
+
+	private void GenerateObject(float width, GameObject parentObject)
+	{
+		for (var i = 0; i < _LineRenderer.positionCount-1; i++)
+		{
+			var vector1 = _LineRenderer.GetPosition(i);
+			var vector2 = _LineRenderer.GetPosition(i + 1);
+
+			_distance = (vector2 - vector1).magnitude;
+			_angle = Mathf.Acos((vector2-vector1).x/_distance) * (180f / Mathf.PI);
+			
+			if (vector2.y < vector1.y)
+				_angle = -_angle;
+			
+			var newObject = Instantiate(_MeshObject, ((vector1 + vector2) / 2f) + new Vector3(0, 5, 3), Quaternion.Euler(0, 0, _angle));
+			newObject.transform.localScale = new Vector3(_distance, width, width);
+			newObject.transform.parent = parentObject.transform;
+		}
+		
+		Destroy(gameObject);
+	}
+
+}
---
+++ Assets/Scripts/LineManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LineManager : MonoBehaviour , IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private RectTransform _RectTransform;
+    [SerializeField] private GameObject _LinePrefab;
+    [SerializeField] private Gradient _LineColor;
+    [SerializeField] private GameObject _MeshParentObject;
+    [Space ( 20f )]
+    [SerializeField] private float _LinePointsMinDistance;
+    [SerializeField] private float _LineWidth;
+
+    private Camera _cam;
+    private Line _currentLine;
+    
+    private Vector3 _tempMousePos;
+    
+    private void Start() 
+    {
+        _cam = Camera.main;
+    }
+
+    private void Update() 
+    {
+        if (_currentLine != null)
+        {
+            Draw();
+        }
+    }
+    
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        BeginDraw();
+    }
+    
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        EndDraw();
+    }
+    
+    private void BeginDraw() 
+    {
+        _currentLine = Instantiate(_LinePrefab, transform).GetComponent<Line>( );
+        
+        _currentLine.SetLineColor (_LineColor);
+        _currentLine.SetPointsMinDistance (_LinePointsMinDistance);
+        _currentLine.SetLineWidth (_LineWidth);
+    }
+    
+    private void Draw()
+    {
+        _tempMousePos = Input.mousePosition;
+        
+        var anchorOffsetX = Screen.width / 2;
+        _tempMousePos.x = Mathf.Clamp(_tempMousePos.x, _RectTransform.offsetMin.x + anchorOffsetX, _RectTransform.offsetMax.x +anchorOffsetX);
+        _tempMousePos.y = Mathf.Clamp(_tempMousePos.y, _RectTransform.offsetMin.y, _RectTransform.offsetMax.y);
+        _tempMousePos.z = 5f;
+        
+        var mousePosition = _cam.ScreenToWorldPoint(_tempMousePos);
+        _currentLine.AddPoint (mousePosition);
+    }
+
+    private void EndDraw()
+    {
+        if (_currentLine == null) return;
+        
+        if (_currentLine.PointsCount < 2)   // If line has one point
+        {
+            Destroy(_currentLine.gameObject);
+        } 
+        else 
+        {
+            _currentLine.DrawObject(_LineWidth, _MeshParentObject);
+            _currentLine = null;
+        }
+    }
+}
